175075ff4aba1015632e1d7684aaa17f8da372ea
Sounak <sounakdas9005@gmail.com>
2022-07-28 09:45:12 +0530

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { PDFGenerator } from '@ionic-native/pdf-generator/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { InvoiceComponent } from './components/invoice/invoice.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, InvoiceComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [PDFGenerator, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { InvoiceComponent } from '../components/invoice/invoice.component';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public order = environment.orderData;
 
-  constructor() {}
+  constructor(private modalCtrl: ModalController) { }
 
+  async openInvoice(order) {
+    const InvoiceModal = await this.createModal(InvoiceComponent, { order });
+    await InvoiceModal.present();
+  }
+
+  async createModal(component, componentProps?, cssClass?): Promise<HTMLIonModalElement> {
+    const modal = await this.modalCtrl.create({
+      component,
+      cssClass,
+      componentProps,
+      backdropDismiss: true
+    });
+    return modal;
+  }
 }
